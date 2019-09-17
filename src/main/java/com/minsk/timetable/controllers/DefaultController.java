9f7464014fae2eeb1ab479433f7f683f7571c099
KotsyubaT <taras32300@yandex.ru>
2019-09-17 15:36:07 +0300

    My second commit

--- src/main/java/com/minsk/timetable/controllers/DefaultController.java
+++ src/main/java/com/minsk/timetable/controllers/DefaultController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author AleksandrovichK
+ * @author AleksandrovichK and KotsyubaT
  */
 @RestController
 public class DefaultController {
