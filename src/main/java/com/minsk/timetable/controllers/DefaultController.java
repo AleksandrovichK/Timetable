1859cfd4dfc349961c810573ee43c05fc3ebebdd
AleksandrovichK <killertom1997@gmail.com>
2019-09-17 15:27:12 +0300

    Rest specification

--- src/main/java/com/minsk/timetable/controllers/DefaultController.java
+++ src/main/java/com/minsk/timetable/controllers/DefaultController.java
@@ -1,5 +1,8 @@
 package com.minsk.timetable.controllers;
 
+import com.minsk.timetable.dto.CustomObject;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,20 @@ public class DefaultController {
         return "Hello!\nAll the services are working now!";
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = "/post-request")
+    public CustomObject greetingsPOST(@RequestBody CustomObject obj) {
+        return new CustomObject("Taras", 34, 34L);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/put-request")
+    public String greetingsPUT() {
+        return "Here comes PUT";
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/del-request")
+    public String greetingsDELETE() {
+        return "Here comes DELETE";
+    }
 
     @RequestMapping(method = RequestMethod.GET, value = "/error")
     public String error() {
---
+++ src/main/java/com/minsk/timetable/dto/CustomObject.java
@@ -0,0 +1,40 @@
+package com.minsk.timetable.dto;
+
+public class CustomObject {
+    private String name;
+    private Integer age;
+    private Long penisLength;
+
+    public CustomObject() {
+    }
+
+    public CustomObject(String name, Integer age, Long penisLength) {
+        this.name = name;
+        this.age = age;
+        this.penisLength = penisLength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Long getPenisLength() {
+        return penisLength;
+    }
+
+    public void setPenisLength(Long penisLength) {
+        this.penisLength = penisLength;
+    }
+}
